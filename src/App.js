b98e2276dfa3dac7db34044f3a97d9a87065f65a
Ankan <ankankarmakar37@gmail.com>
2022-05-23 20:11:33 -0700

    three pages are added

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import India from './pages/India';
+import Sports from './pages/Sports';
+import ScienceAndTech from './pages/ScienceAndTech';
+function App() {
+
+
+  return (
+
+    <>
+       {/* <India></India> */}
+       {/* <Sports></Sports> */}
+       <ScienceAndTech></ScienceAndTech>
+    </>
+  );
+}
+
+export default App;
